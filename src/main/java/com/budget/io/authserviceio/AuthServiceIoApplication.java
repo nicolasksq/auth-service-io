2f4d70e92807e1cda28857d0b7256c569bd7849a
Nicolas Andreoli <nicolas.andreoli@playtika.com>
2020-06-06 19:00:38 -0300

    add redis repository annotation

--- src/main/java/com/budget/io/authserviceio/AuthServiceIoApplication.java
+++ src/main/java/com/budget/io/authserviceio/AuthServiceIoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableRedisRepositories
 public class AuthServiceIoApplication {
 
     public static void main(String[] args) {
